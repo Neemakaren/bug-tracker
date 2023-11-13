174afcf7900772ab1518978b9bff3b0f0a3d3fda
Neema <bun4339@gmail.com>
2023-11-13 16:44:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BugTable from "./BugTable";
+// import { BugPriority, IBug } from "./IBug";
+import { v4 as uuid } from "uuid";
+import { FormEvent, useState } from "react";
 
 function App() {
+  const [newBugDescription, setNewBugDescription] = useState("");
+  const [newBugPriority, setNewBugPriority] = useState("Medium");
+  const [bugList, setBugList] = useState([]);
+
+  const addBug = (event) => {
+    event.preventDefault();
+    const newBug = {
+      id: uuid(),
+      description: newBugDescription,
+      priority: newBugPriority,
+    }
+
+    setBugList(
+      [
+        ...bugList,
+        newBug,
+      ]
+    );
+
+    setNewBugDescription('');
+    setNewBugPriority('Medium');
+  };
+
+  const deleteBug = (id) => {
+    const bugs = bugList.filter(bug => bug.id !== id);
+
+    setBugList(bugs);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Bug Tracker</h1>
+      <BugTable bugs={bugList} onDeleteBug={(id) => deleteBug(id)}></BugTable>
+      <form className="add-new-bug-form" onSubmit={addBug}>
+        <label htmlFor="newBugDescription">New Bug Description: </label>
+        <input data-testid="newbug-description" id="newBugDescription" type="text" onChange={event => setNewBugDescription(event.target.value)} value={newBugDescription} />
+        <label htmlFor="newBugPriority">New Bug Priority: </label>
+        <select onChange={event => setNewBugPriority(event.target.value)} value={newBugPriority}>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+        <button data-testid="add-bug" type="submit">Add New Bug</button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+// import React from 'react';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+
+test('renders the header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/bug tracker/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('it creates a new bug', () => {
+  render(<App />);
+  const inputEl = screen.getByTestId("newbug-description");
+  userEvent.type(inputEl, "test bug");
+  fireEvent.click(screen.getByTestId('add-bug'))
+  expect(screen.getByText(/test bug/i)).toBeInTheDocument();
 });
